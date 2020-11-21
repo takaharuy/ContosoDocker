d536a2a599d522a0e5a3764ad5606a9417fd8231
湯治高治 <takaharu-y@ujit67.onmicrosoft.com>
2020-11-21 11:29:53 +0900

    Initialize DB with test data

--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -14,7 +14,7 @@ namespace ContosoUniversity.Models
         public int EnrollmentId { get; set; }
         public int CourseId { get; set; }
         public int StudentId { get; set; }
-        public Grade? grade { get; set; }
+        public Grade? Grade { get; set; }
 
         public Course Course { get; set; }
         public Student Student { get; set; }
